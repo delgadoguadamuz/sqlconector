ee448708e4538afea54cebcf74da9df0147e8297
delgadoguadamuz <delgadoguadamuz@hotmail.com>
2016-09-08 19:47:50 -0600

    while terminado en estudiante

--- UniversitySystem/UniversitySystem/Data/DataEstudiante.cs
+++ UniversitySystem/UniversitySystem/Data/DataEstudiante.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using UniversitySystem.Entities;
 
 namespace UniversitySystem.Data
 {
@@ -16,18 +17,50 @@ namespace UniversitySystem.Data
 
 
 
-        private DataTable table;
+        private List<Estudiante> list;
 
         public DataEstudiante()
         {
-            table = new DataTable();
             
+            //table = new DataTable();
+            //table.Columns.Add("Id");
+            //table.Columns.Add("Carnet");
+            //table.Columns.Add("Nombre");
+            //table.Columns.Add("Apellido");
+            //table.Columns.Add("Edad");
+            //table.Columns.Add("Sexo");
         }
 
 
-        public DataTable ObtenerTodos()
+        public List<Estudiante> ObtenerTodos()
         {
-            return null;
+            list = new List<Estudiante>();
+            string query = "SELECT * FROM [Estuduante]";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(query,connection);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+
+                Estudiante estudiante = new Estudiante();
+
+                estudiante.Id = (int)reader["Id"];
+                estudiante.Nombre = (string)reader["Nombre"];
+                estudiante.Apellido = (string)reader["Apellido"];
+                estudiante.Carnet = (string)reader["Carnet"];
+                estudiante.Edad = (int)reader["Edad"];
+                estudiante.Sexo = (string)reader["Sexo"];
+
+                list.Add(estudiante);
+            }
+
+            reader.Close();
+            connection.Close();
+
+            return list;
         }
 
 
