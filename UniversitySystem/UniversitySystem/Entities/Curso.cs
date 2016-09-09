4cbeb21b039bdd0c18b6d7f0bee36e5ab35807c0
delgadoguadamuz <delgadoguadamuz@hotmail.com>
2016-09-08 18:41:52 -0600

    commit de variables

---
+++ UniversitySystem/UniversitySystem/Entities/Curso.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversitySystem.Entities
+{
+     public class Curso
+    {
+
+        public int Id { get; set; }
+
+        public int IdProfesor { get; set; }
+
+        public String Sigla { get; set; }
+
+        public String Nombre { get; set; }
+
+        public int CupoMax { get; set; }
+
+    }
+}
---
+++ UniversitySystem/UniversitySystem/Entities/Estudiante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversitySystem.Entities
+{
+    public class Estudiante
+    {
+
+        public int Id { get; set; }
+
+        public String Nombre { get; set; }
+
+        public String Apellido { get; set; }
+
+        public String Sexo { get; set; }
+
+        public String Carnet { get; set; }
+
+        public int Edad { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
