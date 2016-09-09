6c866d6ff1d301f5d03668eeb7fea5d3243d0063
delgadoguadamuz <delgadoguadamuz@hotmail.com>
2016-09-08 19:09:35 -0600

    commit all

---
+++ UniversitySystem/UniversitySystem/Data/DataEstudiante.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace UniversitySystem.Data
+{
+  public class DataEstudiante
+    {
+       private string connectionString =
+                "Data Source=TLCOYO-LOANER7\\SQLEXPRESS;Initial Catalog=Universidad;"
+                + "Integrated Security=true";
+
+
+
+        private DataTable table;
+
+        public DataEstudiante()
+        {
+            table = new DataTable();
+            
+        }
+
+
+        public DataTable ObtenerTodos()
+        {
+            return null;
+        }
+
+
+
+    }
+
+
+
+}
